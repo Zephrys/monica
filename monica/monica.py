fb76d44241ebc58ce358dd4a6ffd897c1e0f34ea
Gyanendra Mishra <anomaly.the@gmail.com>
2016-02-23 01:42:56 +0530

    wrote surprise, cuisine and reviews

--- monica/monica.py
+++ monica/monica.py
@@ -6,9 +6,9 @@ Usage:
   monica surprise
   monica <restaurant-name>
   monica search <restaurant name>
-  monica trending
+  monica reviews
   monica budget <per-person-cost>
-  monica cuisine (chinese | indian | ..)
+  monica cuisine (<name> | list)
   monica config
 
 Options:
@@ -18,29 +18,108 @@ Options:
 """
 
 from docopt import docopt
+from config import config
+from tabulate import tabulate
+import random
 
 __version__ = '0.0.2'
-
+headers = {'Accept' : 'application/json', 'user_key': config.api_key, 'User-Agent': 'curl/7.35.0'}
 
 def surprise():
-  pass
+  url = 'https://developers.zomato.com/api/v2.1/geocode?lat=%s&lon=%s' %(config.lat, config.lon)
+  try:
+    response =requests.get(url, headers = headers)
+  except:
+    print 'Something went wrong!'
+  if response.status_code == 200:
+    data = response.json()
+    restaurants = data['nearby_restaurants']
+    while True:
+      if restaurants == {}:
+        print 'Sorry nothing in your budget :('
+      key = random.choice(restaurants.keys())
+      budget = restaurant[key]['average_cost_for_two']
+      if float(budget)/2 <= config.budget:
+        restaurant = restaurant[key]
+        break
+      else:
+        restaurants.pop(key, None)
+    table = [[restaurant["id"] , restaurant["name"], restaurant["curency"]
+    + " " + str(float(restaurant['average_cost_for_two'])/2)] ,restaurant["phone_numbers"], restaurant["user_rating"]["aggregate_rating"], restaurant["location"]["address"][:50]]
+    print tabulate(table, headers=["ID", "Name", "Budget", "Contact", "Rating", "Address"])
+  else:
+    print 'Something went wrong!'
+
+
 
 def restaurant():
   pass
 
-def cuisine():
-  pass
+def cuisine(cuisine):
+  if cuisine == 'list':
+    url = "https://developers.zomato.com/api/v2.1/cuisines?city_id=%s&lat%s&lon=%s" %(config.city_id, config.lat, config.lon)
+    try:
+      response = requests.get(url, headers=headers)
+    except:
+      print 'Something went wrong!'
+    if response.status_code == 200:
+      data = response.json()
+      cuisines = data['cuisines']
+      cuisine_list = []
+      for cuisine in cuisines:
+        cuisine_list.append(cuisine["cuisine_id"], cuisine["cuisine_name"])
+      print tabulate(cuisine_list, headers=["ID", "Cuisine Name"])
+  else:
+    url = "https://developers.zomato.com/api/v2.1/search?count=10&lat=%s&lon=%s&cuisines=%s&sort=cost" %(config.lat, config.lon, cuisine)
+    try:
+      response= requests.get(url, headers=headers)
+    except:
+      print 'Something went wrong!'
+    if response.status_code == 200:
+      data = response.json()
+      count  = data['results_found']
+      if count == 0:
+        print "Nothing Found!"
+      else:
+        restaurants = data["restaurants"]
+        restaurants_list = []
+        for restaurant in restaurants:
+          restaurants_list.append(restaurant["id"] , restaurant["name"], restaurant["curency"]
+    + " " + str(float(restaurant['average_cost_for_two'])/2)] ,restaurant["phone_numbers"], restaurant["user_rating"]["aggregate_rating"], restaurant["location"]["address"][:50])
+        print tabulate(restaurants_list, headers=["ID", "Name", "Budget", "Contact", "Rating", "Address"])
+    else:
+      print "Something went wrong!"
 
 def search():
   pass
 
-def trending():
-  pass
+def reviews(id):
+  url = "https://developers.zomato.com/api/v2.1/reviews?res_id=%s&count=5"%(id)
+  try:
+    response = requests.get(url, headers=headers)
+  except:
+    print 'Something went wrong!'
+  if response.status_code == 200:
+    data = response.json()
+    count= data["reviews_count"]
+    if count == 0:
+      print 'No Reviews!'
+    else:
+      for review in data["user_reviews"]:
+        print "--------------"
+        print review["rating"]
+        print review["rating_text"]
+        print review["review_text"]
+        print review["review_time_friendly"]
+        print "--------------"
+  else:
+    print 'Something went wrong'
+
 
 def budget():
   pass
 
 def config():
-  pass
+  config.configure()
 
 def main():
