572e0aaf0798c4a486b093ae726897db9a4f6be5
Gyanendra Mishra <anomaly.the@gmail.com>
2016-02-22 23:39:40 +0530

    added doccumentation to monica

---
+++ monica/monica.py
@@ -0,0 +1,21 @@
+r"""
+
+monica helps you order food from the terminal
+
+Usage:
+  monica surprise
+  monica <restaurant-name>
+  monica search <restaurant name>
+  monica trending
+  monica budget <per-person-cost>
+
+Options:
+  -h --help     Show this screen.
+  --version     Show version.
+
+"""
+
+from docopt import docopt
+
+__version__ = '0.0.2'
+
